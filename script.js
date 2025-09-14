1e1250044ecd41598412a7ebec89c6675bb94430
M Adnan <exadnan61@gmail.com>
2025-09-14 07:52:24 +0500

    first upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Typed headline
+new Typed("#typed", {
+  strings: [
+    "Software Engineer",
+    " Full Stack Web Developer",
+    "Python and Django Learner",
+  
+  ],
+  typeSpeed: 70,
+  backSpeed: 40,
+  backDelay: 1200,
+  smartBackspace: true,
+  loop: true
+});
+
+// AOS animations
+AOS.init({
+  duration: 900,
+  once: true,
+  offset: 80
+});
+
+
